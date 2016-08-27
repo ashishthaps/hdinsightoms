7f9c68bd628bf8a1714c7ec2a3f5f2753787186c
Ashish Thapliyal <ashishth@microsoft.com>
2016-08-26 22:50:20 -0700

    cluster name 2

--- install-hbase-monitor.sh
+++ install-hbase-monitor.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagentx64.sh
 sudo sh /tmp/omsagentx64.sh --upgrade -w $1 -s $2
-wget -O /etc/opt/microsoft/omsagent/plugin/filter_hdinsight.rb https://github.com/ashishthaps/hdinsightoms/blob/master/filter_hdinsight.rb
-wget -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.conf https://github.com/ashishthaps/hdinsightoms/blob/master/hbase.conf
+sudo wget -O /etc/opt/microsoft/omsagent/plugin/filter_hdinsight.rb https://github.com/ashishthaps/hdinsightoms/blob/master/filter_hdinsight.rb
+sudo wget -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.conf https://github.com/ashishthaps/hdinsightoms/blob/master/hbase.conf
 sudo service omsagent restart
 
