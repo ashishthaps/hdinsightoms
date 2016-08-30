28b98eb82bd02e83cac85b23c8b4872d1e1abf2d
Ashish Thapliyal <ashishthaps@hotmail.com>
2016-08-29 17:00:43 -0700

    updated path issues

--- install-hbase-monitor.sh
+++ install-hbase-monitor.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade -w $1 -s $2
 sudo wget -O /etc/opt/microsoft/omsagent/plugin/filter_hdinsight.rb https://github.com/ashishthaps/hdinsightoms/blob/master/filter_hdinsight.rb
